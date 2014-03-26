45ebdf3119cead2c2246ad9acf7e72e7dce810ef
James Koppen <jk@jameskoppen.com>
2014-03-26 01:26:02 -0500

    tabs in index generator for a nice view source... turd-polishing.

--- index-html.sh
+++ index-html.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 # cribbed from http://odoepner.wordpress.com/2012/02/17/shell-script-to-generate-simple-index-html/
 
-echo '<html><head><title>Four Means Index</title></head>'
-echo '<body><ul>'
-sed 's/^.*/<li><a href="&">&<\/a><\/li>/'
-echo '</ul></body></html>'
+echo '<html>'
+echo "\t<head>"
+echo "\t\t<title>Four Means Index</title>"
+echo "\t</head>"
+echo "\t<body>"
+echo "\t\t<p><a href='https://github.com/jiko/four-means'>source code</a></p>"
+echo "\t\t<ul>"
+sed 's/^.*/\t\t\t<li><a href="&">&<\/a><\/li>/'
+echo "\t\t</ul>"
+echo "\t</body>"
+echo "</html>"
