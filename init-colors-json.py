ffa61c1c87f5befb3388b159b4c8999ee9a6d336
James Koppen <jk@jameskoppen.com>
2014-04-05 09:12:51 -0500

    Simpler data structure win.

--- init-colors-json.py
+++ init-colors-json.py
@@ -5,15 +5,13 @@ import json
 pages = listdir("./html")
 pages.remove('index.html')
 pages.remove('list.html')
-combos = []
+combos = dict()
 
 for page in pages:
     if page[-4:] == 'html':
-      colors = dict()
       html = BeautifulSoup(open('./html/'+page))
       hexes = [html.select(div)[0]["style"].replace("background-color:","") for div in ["#one","#two","#three","#four"]]
-      colors[html.title.string.strip()] = hexes
-      combos.append(colors)
+      combos[html.title.string.strip()] = hexes
 
 filename = path.join("html", "colors.json")
 with open(filename, 'w') as f:
